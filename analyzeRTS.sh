f8425300fc69f96a012df91df538f1bebabcc07c
Karim Abrik <qkarim99@hotmail.com>
2023-06-06 19:53:39 +0200

    analyzeRTS-Skript aktualisiert - nichtausführbare Commits werden korrekt gelesen

--- analyzeRTS.sh
+++ analyzeRTS.sh
@@ -23,24 +23,25 @@ function getOverallCommits {
 	allNotRunning=0
 	allCommitsWithChange=0
 	allCoveredCommits=0
-	allRuns=0
+nonrunning750to1010=$(cat Commit750bis1010/results/temp_gradoop/nonRunning_gradoop.json | jq '.nonRunableReasons | to_entries[] | select(.value == "Commit is not running.") | .key' | wc -l)
+nonrunning500to750=$(cat Commit500bis750/results/temp_gradoop/nonRunning_gradoop.json | jq '.nonRunableReasons | to_entries[] | select(.value == "Commit is not running.") | .key' | wc -l)
+nonrunning245to500=$(cat Commit245bis500/results/temp_gradoop/nonRunning_gradoop.json | jq '.nonRunableReasons | to_entries[] | select(.value == "Commit is not running.") | .key' | wc -l)
+
 	for project in Commit1bis194 Commit194bis245 Commit245bis500 Commit500bis750 Commit750bis1010
 	do
 		selectionFile="$project/results/staticTestSelection_*.json"
 		commits=$(cat $selectionFile | jq ".commits | keys | length")
 		tests=$(cat $selectionFile | jq ".initialcommit.initialDependencies | keys | length")
-		notRunning=$(cat $selectionFile | jq ".commits" | grep "\"running\": false" | wc -l)
+		notCompiling=$(cat $selectionFile | jq ".commits" | grep "\"running\": false" | wc -l)
 		commitsWithChange=$(cat $selectionFile | jq ".commits[].changedClazzes | select(length > 0) | length" | wc -l)
 		coveredChanges=$(cat $project/results/traceTestSelection_*.json  | jq ".commits[].testcases | select(length>0) | length" | wc -l)
-		runs=$(cat $project/run-$project*sh | wc -l)
 		allCommits=$(($allCommits+$commits))
 		allTests=$(($allTests+$tests))
-		allNotRunning=$(($allNotRunning+$notRunning))
+		allNotRunning=$(($allNotRunning+$notCompiling))
 		allCommitsWithChange=$(($allCommitsWithChange+$commitsWithChange))
 		allCoveredCommits=$(($allCoveredCommits+$coveredChanges))
-		allRuns=$(($allRuns+$runs))
 	done
-	echo "Tests: $allTests Commits: $allCommits Unausführbar: $allNotRunning Commits mit Änderung: $allCommitsWithChange Abgedeckte Änderungen: $allCoveredCommits Runs: $allRuns"
+	echo "Tests: $allTests Commits: $allCommits Unausführbar: $(($allNotRunning+$nonrunning245to500+$nonrunning500to750+$nonrunning750to1010)) Commits mit Änderung: $allCommitsWithChange Abgedeckte Änderungen: $allCoveredCommits"
 }
 
 getOverallCommits
@@ -61,10 +62,13 @@ do
 	echo "=============="
 	echo $project
 	echo -n "Measured Commits "
-	var1 = "$(cat $project/measurement-results/FirstExecution/statistics.json | jq ".statistics | keys | length")"
-	var2 = "$(cat $project/measurement-results/SecondExecution/statistics.json | jq ".statistics | keys | length")"
-	var1 + var2
+	var1=$(cat $project/measurement-results/FirstExecution/statistics.json | jq ".statistics | keys | length")
+	var2=$(cat $project/measurement-results/SecondExecution/statistics.json | jq ".statistics | keys | length")
+	echo $(($var1+$var2))" First: $var1 Second: $var2"
 	echo -n "Commits containing changes "
-	cat $project/measurement-results/changes.json | jq ".commitChanges | keys | length"
+	changes1=$(cat $project/measurement-results/FirstExecution/changes.json | jq ".commitChanges | keys | length")
+	changes2=$(cat $project/measurement-results/SecondExecution/changes.json | jq ".commitChanges | keys | length")
+	echo $(($changes1+$changes2))
 done
+
 
